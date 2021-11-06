30bc829d96dc6389d39f19cc5e932831e2268a84
Flavien Hardy <flav.hardy@gmail.com>
2021-11-06 08:27:25 -0400

    Update dev scripts

--- test/genconf.sh
+++ test/genconf.sh
@@ -32,19 +32,19 @@ EOF
 
 [ -e ${CURR_DIR}/generated/ssl/key-${cert}.pem ] || openssl genrsa -out ${CURR_DIR}/generated/ssl/key-${cert}.pem 2048 >/dev/null
 [ -e ${CURR_DIR}/generated/ssl/csr-${cert}.pem ] || openssl req -new -key ${CURR_DIR}/generated/ssl/key-${cert}.pem \
-    -out ${CURR_DIR}/generated/ssl/csr-${cert}.pem -subj "/CN=kubernetes" -config ${CURR_DIR}/generated/ssl/req-${cert}.cnf >/dev/null
+    -out ${CURR_DIR}/generated/ssl/csr-${cert}.pem -subj "/CN=system:node:loginapp;/O=system:nodes" -config ${CURR_DIR}/generated/ssl/req-${cert}.cnf >/dev/null
 
 kubectl get csr ${cert} >/dev/null || cat <<EOF | kubectl create -f -
-apiVersion: certificates.k8s.io/v1beta1
+apiVersion: certificates.k8s.io/v1
 kind: CertificateSigningRequest
 metadata:
   name: ${cert}
 spec:
+  signerName: kubernetes.io/kubelet-serving
   request: $(base64 -i ${CURR_DIR}/generated/ssl/csr-${cert}.pem -w0)
   usages:
-  - digital signature
   - key encipherment
-  - client auth
+  - digital signature
   - server auth
 EOF
 echo "OK"
@@ -81,46 +81,54 @@ done
 echo "Generating dex and loginapp configurations"
 
 ### Dex
-cat <<EOF > ${CURR_DIR}/kubernetes/generated/dex-config.yaml
----
-kind: ConfigMap
-apiVersion: v1
-metadata:
-  name: dex
-  namespace: kube-system
-data:
-  config.yaml: |
-    issuer: https://dex.${NODE_IP}.nip.io:32000
-    storage:
-      type: kubernetes
-      config:
-        inCluster: true
-    web:
-      https: 0.0.0.0:5556
-      tlsCert: /etc/dex/tls/tls.crt
-      tlsKey: /etc/dex/tls/tls.key
-    oauth2:
-      skipApprovalScreen: true
+cat <<EOF > ${CURR_DIR}/helm/generated/dex-overrides.yaml
+fullnameOverride: dex
+https:
+  enabled: true
+service:
+  type: NodePort
+  ports:
+    https:
+      nodePort: 32000
+volumes:
+  - name: tls
+    secret:
+      secretName: dex-certs
+volumeMounts:
+  - name: tls
+    mountPath: /etc/dex/tls
+config:
+  issuer: https://dex.${NODE_IP}.nip.io:32000
+  storage:
+    type: kubernetes
+    config:
+      inCluster: true
+  web:
+    https: 0.0.0.0:5554
+    tlsCert: /etc/dex/tls/tls.crt
+    tlsKey: /etc/dex/tls/tls.key
+  oauth2:
+    skipApprovalScreen: true
 
-    staticClients:
-    - id: loginapp
-      redirectURIs:
-      - 'https://loginapp.${NODE_IP}.nip.io:32001/callback'
-      name: 'Loginapp Kube'
-      secret: ZXhhbXBsZS1hcHAtc2VjcmV0
-    - id: loginapp-local
-      redirectURIs:
-      - 'https://loginapp.127.0.0.1.nip.io:8443/callback'
-      name: 'Loginapp local'
-      secret: ZXhhbXBsZS1hcHAtc2VjcmV1
+  staticClients:
+  - id: loginapp
+    redirectURIs:
+    - 'https://loginapp.${NODE_IP}.nip.io:32001/callback'
+    name: 'Loginapp Kube'
+    secret: ZXhhbXBsZS1hcHAtc2VjcmV0
+  - id: loginapp-local
+    redirectURIs:
+    - 'https://loginapp.127.0.0.1.nip.io:8443/callback'
+    name: 'Loginapp local'
+    secret: ZXhhbXBsZS1hcHAtc2VjcmV1
 
-    enablePasswordDB: true
-    staticPasswords:
-    - email: "admin@example.com"
-      # bcrypt hash of the string "password"
-      hash: "\$2a\$10\$2b2cU8CPhOTaGrs1HRQuAueS7JTT5ZHsHSzYiFPm1leZck7Mc8T4W"
-      username: "admin"
-      userID: "08a8684b-db88-4b73-90a9-3cd1661f5466"
+  enablePasswordDB: true
+  staticPasswords:
+  - email: "admin@example.com"
+    # bcrypt hash of the string "password"
+    hash: "$2a$10$2b2cU8CPhOTaGrs1HRQuAueS7JTT5ZHsHSzYiFPm1leZck7Mc8T4W"
+    username: "admin"
+    userID: "08a8684b-db88-4b73-90a9-3cd1661f5466"
 EOF
 
 ### Loginapp
@@ -154,9 +162,25 @@ data:
         server: https://myfakecluster.org
         certificate-authority: |
           -----BEGIN CERTIFICATE-----
-          MIIC/zCCAeegAwIBAgIULkYvGJPRl50tMoVE4BNM0laRQncwDQYJKoZIhvcNAQEL
-          BQAwDzENMAsGA1UEAwwEbXljYTAeFw0xOTAyMTgyMjA5NTJaFw0xOTAyMjgyMjA5
-          NTJaMA8xDTALBgNVBAMMBG15Y2EwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEK
+          MIIDZzCCAk+gAwIBAgIRAM/Oqk7538VUzHwMZ+x7eN0wDQYJKoZIhvcNAQELBQAw
+          FTETMBEGA1UEAxMKa3ViZXJuZXRlczAeFw0yMTExMDUxMDUyMDdaFw0yMjExMDUx
+          MDUyMDdaMDcxFTATBgNVBAoTDHN5c3RlbTpub2RlczEeMBwGA1UEAwwVc3lzdGVt
+          Om5vZGU6bG9naW5hcHA7MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA
+          yc4mV0leq+fJ5KsF6yZTKtWRVzwcZGeYZv3gf1R9zSdexshOtBORRaNHJmuAu0st
+          aHunVPlsSInBGBf4pO4OTLilu965/OQOvFvIFLzl3LNMoHOvUMofJ8lwBlcczil+
+          UYf+xvDNYOYN3Q8WIBs5W2yHgDdFdfb9Z/X6ezL31T0/1+VYXR5cSawIKP9zEY/z
+          miUWqrqRVZN0qvFYp5phMUbcIlNknEo/p0nUXfnz87P/7862BJuVaJVZmAMcg3IS
+          q9Xl27P4NVnIvI5WPrlNrDNv3AupEJFS0c/Gms3nr6BZWNg5tM2mRhAr1Gfm37me
+          fFqM0gxJn2sHjSF5y+M0jQIDAQABo4GPMIGMMA4GA1UdDwEB/wQEAwIFoDATBgNV
+          HSUEDDAKBggrBgEFBQcDATAMBgNVHRMBAf8EAjAAMB8GA1UdIwQYMBaAFOw9vdeK
+          hC8ALh7H+2QYpXPUWUU6MDYGA1UdEQQvMC2CFWRleC4xNzIuMTcuMC4yLm5pcC5p
+          b4IUZGV4LjEyNy4wLjAuMS5uaXAuaW8wDQYJKoZIhvcNAQELBQADggEBAHgdgQ07
+          YXERpAw8wRxxxX6Ozsod/vKww6E5Q4KmU8FxTqkx7hxQpp3neC6b5+yrsbGw2h0R
+          8U9Kog+oNLtoN9K9AqVhbP/t6Ny8XCuEx/WXq01jBfwcrP4BCFD7oOfK5V5Ah5ey
+          11KzZ10tsUTlNXmqVFUr93tCy5Yf4iK8k0SJpxWPcoltzWCG1H5l/j5frlK8AbmD
+          Yn3zbspD+UG7wEbZWM4SFXEDC4DpEOMtRaEDFZPXa5zcSnnCUxsFIbmEPun6Wk1t
+          0qICrfqPjH98dOqfu5+IBb6EoCrTkp9t9ic9hrkdS5CYBvwZHJf+a5/rmte+xyix
+          pvbI7bJ+Sw8Sx2E=
           -----END CERTIFICATE-----
         insecure-skip-tls-verify: false
 ---
@@ -195,9 +219,25 @@ clusters:
     server: https://myfakecluster.org
     certificate-authority: |
       -----BEGIN CERTIFICATE-----
-      MIIC/zCCAeegAwIBAgIULkYvGJPRl50tMoVE4BNM0laRQncwDQYJKoZIhvcNAQEL
-      BQAwDzENMAsGA1UEAwwEbXljYTAeFw0xOTAyMTgyMjA5NTJaFw0xOTAyMjgyMjA5
-      NTJaMA8xDTALBgNVBAMMBG15Y2EwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEK
+      MIIDZzCCAk+gAwIBAgIRAM/Oqk7538VUzHwMZ+x7eN0wDQYJKoZIhvcNAQELBQAw
+      FTETMBEGA1UEAxMKa3ViZXJuZXRlczAeFw0yMTExMDUxMDUyMDdaFw0yMjExMDUx
+      MDUyMDdaMDcxFTATBgNVBAoTDHN5c3RlbTpub2RlczEeMBwGA1UEAwwVc3lzdGVt
+      Om5vZGU6bG9naW5hcHA7MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA
+      yc4mV0leq+fJ5KsF6yZTKtWRVzwcZGeYZv3gf1R9zSdexshOtBORRaNHJmuAu0st
+      aHunVPlsSInBGBf4pO4OTLilu965/OQOvFvIFLzl3LNMoHOvUMofJ8lwBlcczil+
+      UYf+xvDNYOYN3Q8WIBs5W2yHgDdFdfb9Z/X6ezL31T0/1+VYXR5cSawIKP9zEY/z
+      miUWqrqRVZN0qvFYp5phMUbcIlNknEo/p0nUXfnz87P/7862BJuVaJVZmAMcg3IS
+      q9Xl27P4NVnIvI5WPrlNrDNv3AupEJFS0c/Gms3nr6BZWNg5tM2mRhAr1Gfm37me
+      fFqM0gxJn2sHjSF5y+M0jQIDAQABo4GPMIGMMA4GA1UdDwEB/wQEAwIFoDATBgNV
+      HSUEDDAKBggrBgEFBQcDATAMBgNVHRMBAf8EAjAAMB8GA1UdIwQYMBaAFOw9vdeK
+      hC8ALh7H+2QYpXPUWUU6MDYGA1UdEQQvMC2CFWRleC4xNzIuMTcuMC4yLm5pcC5p
+      b4IUZGV4LjEyNy4wLjAuMS5uaXAuaW8wDQYJKoZIhvcNAQELBQADggEBAHgdgQ07
+      YXERpAw8wRxxxX6Ozsod/vKww6E5Q4KmU8FxTqkx7hxQpp3neC6b5+yrsbGw2h0R
+      8U9Kog+oNLtoN9K9AqVhbP/t6Ny8XCuEx/WXq01jBfwcrP4BCFD7oOfK5V5Ah5ey
+      11KzZ10tsUTlNXmqVFUr93tCy5Yf4iK8k0SJpxWPcoltzWCG1H5l/j5frlK8AbmD
+      Yn3zbspD+UG7wEbZWM4SFXEDC4DpEOMtRaEDFZPXa5zcSnnCUxsFIbmEPun6Wk1t
+      0qICrfqPjH98dOqfu5+IBb6EoCrTkp9t9ic9hrkdS5CYBvwZHJf+a5/rmte+xyix
+      pvbI7bJ+Sw8Sx2E=
       -----END CERTIFICATE-----
     insecure-skip-tls-verify: false
 EOF
@@ -222,7 +262,7 @@ config:
     # This will generate a self-signed certificate
     enabled: true
     altnames:
-      - "loginapp.${NODE_IP}.nip.io"
+      - "loginapp.172.17.0.2.nip.io"
   clusters:
     - name: myfakecluster
       server: https://myfakecluster.org
@@ -239,25 +279,34 @@ configOverwrites:
     mainUsernameClaim: "name"
 dex:
   enabled: true
+  fullnameOverride: dex
   replicas: 2
-  https: true
-  grpc: false
+  https:
+    enabled: true
   service:
     type: NodePort
-  ports:
-    web:
-      containerPort: 5556
-      # for service.type: NodePort
-      nodePort: 32000
-      servicePort: 32000
-  certs:
-    web:
-      # This will generate a self-signed certificate
-      create: true
-      altNames:
-      - dex.${NODE_IP}.nip.io
+    ports:
+      https:
+        nodePort: 32000
+  volumes:
+    - name: tls
+      secret:
+        secretName: dex-certs
+  volumeMounts:
+    - name: tls
+      mountPath: /etc/dex/tls
   config:
     issuer: https://dex.${NODE_IP}.nip.io:32000
+    storage:
+      type: kubernetes
+      config:
+        inCluster: true
+    web:
+      https: 0.0.0.0:5554
+      tlsCert: /etc/dex/tls/tls.crt
+      tlsKey: /etc/dex/tls/tls.key
+    oauth2:
+      skipApprovalScreen: true
     staticClients:
     - id: loginapp
       redirectURIs:
@@ -274,7 +323,7 @@ dex:
     staticPasswords:
     - email: "admin@example.com"
       # bcrypt hash of the string "password"
-      hash: "\$2a\$10\$2b2cU8CPhOTaGrs1HRQuAueS7JTT5ZHsHSzYiFPm1leZck7Mc8T4W"
+      hash: "a0b2cU8CPhOTaGrs1HRQuAueS7JTT5ZHsHSzYiFPm1leZck7Mc8T4W"
       username: "admin"
       userID: "08a8684b-db88-4b73-90a9-3cd1661f5466"
 EOF
