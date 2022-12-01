9eefef1d0da3a02e069d98201d2781913ab28d94
BORYSENKO, DMYTRO <db821n@exo.att.com>
2022-11-30 20:47:08 -0800

    -- adjustments to run tests on macos

--- test/kubernetes/kindup.sh
+++ test/kubernetes/kindup.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
 CURR_DIR=$(dirname $0)
+if [[ $OSTYPE == 'darwin'* ]]; then
+  cat << EOF > ${CURR_DIR}/generated/kind-cluster.yaml
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  image: kindest/node:v1.22.2
+  extraPortMappings:
+    - containerPort: 32000
+      hostPort: 32000
+EOF
+else
+  cat << EOF > ${CURR_DIR}/generated/kind-cluster.yaml
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  image: kindest/node:v1.22.2
+EOF
+fi
 
-kind create cluster --name=loginapp --config=${CURR_DIR}/kind-cluster.yaml
+kind create cluster --name=loginapp --config=${CURR_DIR}/generated/kind-cluster.yaml
